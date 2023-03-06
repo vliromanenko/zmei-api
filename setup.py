8c79ab5ab1e9c9c896c614828efdbdce2ad36d45
Vlad Romanenko <romanenkovli@yandex.ru>
2023-03-06 13:47:43 +0300

    Material class bug fixes

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open('HISTORY.md') as history_file:
     HISTORY = history_file.read()
 
 setup_args = dict(
-    name='zmei-api',
-    version='0.0.1',
+    name='zmeiapi',
+    version='0.0.3',
     description='Useful tools to work with Zmei calculation code',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
@@ -17,13 +17,13 @@ setup_args = dict(
     author='Vladislav Romanenko',
     author_email='vliromanenko@yandex.ru',
     keywords=['zmei-api'],
-    url='https://pypi.org/project/zmei-api/',
-    download_url='https://pypi.org/project/zmei-api/'
+    url='https://github.com/vliromanenko/zmei-api',
+    download_url='https://pypi.org/project/zmeiapi/'
 )
 
 install_requires = [
-    'pickle'
+    
 ]
 
 if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
+    setup(**setup_args, install_requires=None)
