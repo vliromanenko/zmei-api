e8eefa223733811fe2277b117627a9415afac9ad
Vlad Romanenko <romanenkovli@yandex.ru>
2023-03-14 03:52:09 +0300

    BumatMaterialReader was transported into BumatReader file

--- zmeiapi/BumatReader.py
+++ zmeiapi/BumatReader.py
@@ -4,8 +4,35 @@ __author__ = 'Vlad Romanenko'
 
 import os
 import glob
-from BumatObject import BumatMaterialObject
 from Logger import logger
+
+
+class BumatMaterialObject:
+    def __init__(
+            self,
+            material_name,
+            burnup,
+            burnup_days,
+            concentrations_sum,
+            vol,
+            nuclides,
+            concentrations
+    ):
+        self.burnup = burnup
+        self.burnup_days = burnup_days
+        self.material_name = material_name
+        self.concentrations_sum = concentrations_sum
+        self.vol = vol
+
+        self.nuclides = nuclides
+        self.concentrations = concentrations
+
+        self.nuclides_and_concentrations = {}
+        for nuclide, concentration in zip(self.nuclides, self.concentrations):
+            self.nuclides_and_concentrations[nuclide] = concentration
+
+    def __str__(self):
+        return f'BumatObject {self.material_name}, burnup = {self.burnup} MWd/kgU'
 
 
 class BumatReader:
