56147fbbedf613285408fdd5966d58fcfae7384b
Vlad Romanenko <romanenkovli@yandex.ru>
2023-03-14 04:03:37 +0300

    minor updates

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='zmeiapi',
-    version='0.0.7',
+    version='0.0.9',
     description='Useful tools to work with Zmei calculation code',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
